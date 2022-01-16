b215ac694bdc16aaf148d6610409998cb9fbe17f
Lukasz Galas <galaslukasz1998@gmail.com>
2022-01-16 21:32:34 +0100

    Improved generating random tests.

--- testgenerator.cpp
+++ testgenerator.cpp
@@ -28,11 +28,26 @@ void createQuestions(int n, long long int univ)
 map<string, createFunction> funcList{
   {"data", createData}, {"questions", createQuestions}};
 
+size_t sysrandom(void* dst, size_t dstlen)
+{
+  char* buffer = reinterpret_cast<char*>(dst);
+  ifstream stream("/dev/urandom", std::ios_base::binary | std::ios_base::in);
+  stream.read(buffer, dstlen);
+
+  return dstlen;
+}
+
 int main(int argc, char *argv[])
 {
   map<string, createFunction>::iterator ele;
   int n;
   long long int univ;
+
+  array<mt19937::UIntType, mt19937::state_size> state;
+  sysrandom(state.begin(), state.length*sizeof(mt19937::UIntType));
+  seed_seq s(state.begin(), state.end());
+
+  random_generator.seed(s)
 
   FAIL(argc != 4);
 
